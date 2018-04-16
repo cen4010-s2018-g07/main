64ce6ea41bde6c972b8ea5c5eaf1b569fab22037
Kaan Tasbas <mktasbas@gmail.com>
2018-04-15 20:38:46 -0400

    Began login.php

---
+++ src/group/login-register/login.php
@@ -0,0 +1,14 @@
+// M. Kaan Tasbas
+
+/* form ids
+ login_username: inputUsernameEmail
+ login_password: inputPassword
+
+ register_firstname: firstName
+ register_lastname: lastName
+ register_email: email
+ register_znumber: zNumber
+ register_phonenumber: phoneNumber
+ register_password: password
+ register_college: college
+*/
